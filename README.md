6c57afc588e329cd74f54f47c2f46334444e52ee
YanagiEiichi <admin@web-tinker.com>
2015-04-20 19:59:23 +0800

    0.1.0

--- README.md
+++ README.md
@@ -3,19 +3,23 @@
 #### Usage
 
 ```javascript
-objectStorage('LocalStorageKey', function(data) {
-  // Save data on next tick
-  data.test = 123;
-});
+var object = objectStorage('LocalStorageKey');
+
+// Save automatically data on next tick
+data.test = 123;
 ```
 
 ```javascript
-objectStorage('LocalStorageKey', function(data) {
-  data.test = 456;
-  // Save data on next tick after promise resolved
-  return new Promise(function(resolve) {
-    setTimeout(resolve, 1000);
-  });
+var object1 = objectStorage('LocalStorageKey');
+var object2 = objectStorage('LocalStorageKey');
+
+// The result will be cache on the same tick
+console.log(object1 === object2); // true
+
+setTimeout(function() {
+  var object3 = objectStorage('LocalStorageKey');
+  // The result will not be cache on different tick
+  console.log(object2 === object3); // false
 });
 ```
 
--- objectstorage.js
+++ objectstorage.js
@@ -1,18 +1,35 @@
-var objectStorage = function(key, callback) {
-  if(typeof callback !== 'function') throw new Error('Type Error: The second param "callback" MUST BE a function');
-  var object;
-  Promise.resolve().then(function() {
-    // Try to parse JSON from localStorage
-    var value = localStorage.getItem(key) || '';
-    object = JSON.parse(value);
-    if(!object || typeof object !== 'object') throw 0;
-    return object;
-  }).then(null, function() {
-    // Create a new object on JSON parse error
-    return object = {};
-  }).then(callback).then(function() {
-    // Save finally JSON to localStorage
-    localStorage.setItem(key, JSON.stringify(object));
-  });
-};
+var objectStorage;
+
+void function() {
+
+  var cache = {};
+
+  objectStorage = function(key) {
+
+    var object;
+
+    // Try to read from cache
+    if(key in cache) return cache[key];
+
+    // Try to parse synchronously JSON from localStorage
+    try {
+      object = JSON.parse(localStorage.getItem(key));
+      if(!object || typeof object !== 'object') throw 0;
+    } catch(error) {
+      // Create a new object on JSON parse error
+      object = {};
+    }
+
+    // Save to localStorage and remove cache on next tick
+    setTimeout(function() {
+      delete cache[key];
+      localStorage.setItem(key, JSON.stringify(object));
+    });
+
+    // Cache returnd object
+    return cache[key] = object;
+
+  };
+
+}();
 
